3c7dcfff284f8819b4e99669ee5c7b6c18d6681c
Carlos Borges <kaca_master@hotmail.com>
2024-11-13 00:41:01 -0300

    Criação de HOOKS e funcionalidade de mudar de etapas

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+// components
+import {GrFormNext, GrFormPrevious, GrEmoji, GrAmazon, GrApple} from 'react-icons/gr'
+import {FiSend} from 'react-icons/fi';
+import UserForm from './components/UserForm';
+import ReviewForm from './components/ReviewForm';
+import Thanks from './components/Thanks';
+import './App.css'
+
+// Hooks
+import { useForm } from './hooks/useForm';
+
+
+function App() {
+
+  const formComponents = [
+    <UserForm/>,
+    <ReviewForm/>,
+    <Thanks/>
+  ]
+
+  const {currentStep, currentComponent, changeStep, isLastStep, isFirststep} = useForm(formComponents)
+
+ 
+  return (
+    <>
+    <div className='app'>
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo para avliar o produto
+        </p>
+      </div>
+      <div className="form-container">
+        <p>etapas</p>
+        <form onSubmit={(e) => changeStep(currentStep +1, e)}>
+          <div className="inputs-container">{currentComponent}</div>
+          <div className="action">
+            {!isFirststep && (<button type='button' onClick={() => changeStep(currentStep -1)}>
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>)}
+           {!isLastStep ? (
+            <button type="submit">
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+            ) : ( 
+              <button type="button">
+              <span>Enviar</span>
+              <FiSend />
+            </button>
+            )}
+          </div>
+        </form>
+      </div>
+      
+    </div>
+      
+        
+    </>
+  )
+}
+
+export default App
---
+++ src/components/UserForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserForm() {
+  return (
+    <div>
+        <h2>User form</h2>
+    </div>
+  )
+}
+
+export default UserForm
